7472c566f37300bea51e15982cf5166527086064
Princess-Nnamdi <pnnamdi2020@gmail.com>
2023-05-30 18:08:26 -0700

    final touches

--- functions-1.c
+++ functions-1.c
@@ -13,7 +13,7 @@
  **/
 
 int print_unsigned(va_list types, char buffer[],
-int flags, int width, int precision, inst size)
+int flags, int width, int precision, int size)
 {
 	int k = BUFF_SIZE - 2;
 	unsigned long int numb = va_arg(types, unsigned long int);
@@ -30,7 +30,7 @@ int flags, int width, int precision, inst size)
 		numb /= 10;
 	}
 	k++;
-	return (write_unsignd(0, k, buffer, flags, width, precision, size));
+	return (write_unsgnd(0, k, buffer, flags, width, precision, size));
 }
 
 
@@ -63,7 +63,7 @@ int flags, int width, int precision, int size)
 	while (numb > 0)
 	{
 		buffer[j--] = (numb % 8) + '0';
-		num /= 8;
+		numb /= 8;
 	}
 	if (flags & F_HASH && init_numb != 0)
 	{
@@ -85,8 +85,8 @@ int flags, int width, int precision, int size)
  * Return: number of chars printed
  **/
 
-int print_hexadeciaml(va_list types, char buffer[],
-int flags, int width, int precision, inst size)
+int print_hexadecimal(va_list types, char buffer[],
+int flags, int width, int precision, int size)
 {
 	return (print_hexa(types, "0123456789abcdef", buffer,
 	flags, 'x', width, precision, size));
@@ -106,7 +106,7 @@ int flags, int width, int precision, inst size)
  **/
 
 int print_hexa_upper(va_list types, char buffer[],
-int flags, int width, int precision, inst size)
+int flags, int width, int precision, int size)
 {
 	return (print_hexa(types, "0123456789ABCDEF", buffer,
 	flags, 'X', width, precision, size));
@@ -140,6 +140,7 @@ int flags, char flag_ch, int width, int precision, int size)
 	{
 		buffer[i--] = '0';
 		buffer[BUFF_SIZE - 1] = '\0';
+	}
 	while (num > 0)
 	{
 		buffer[i--] = map_to[num % 16];
