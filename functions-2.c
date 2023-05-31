ce0ff84b97d92883f4fab270cc5c94ec7ea60208
Sarabhm <rahal.saraa@gmail.com>
2023-05-30 17:42:10 -0700

    teest

--- functions-2.c
+++ functions-2.c
@@ -81,3 +81,86 @@ j++;
 buffer[j + offset] = '\0';
 return (write(1, buffer, j + offset));
 }
+/**
+ * print_reverse - Prints reverse string.
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Numbers of chars printed
+ */
+int print_reverse(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+char *str;
+int i, count = 0;
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(size);
+str = va_arg(types, char *);
+if (str == NULL)
+{
+UNUSED(precision);
+str = ")Null(";
+}
+for (i = 0; str[i]; i++)
+;
+for (i = i - 1; i >= 0; i--)
+{
+char z = str[i];
+write(1, &z, 1);
+count++;
+}
+return (count);
+}
+/**
+ * print_rot13string - Print a string in rot13.
+ * @types:  arguments
+ * @buffer: Buffer
+ * @flags: Calculating active flags
+ * @width: width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Numbers of character printed
+ */
+int print_rot13string(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+char w;
+char *str;
+unsigned int i, j;
+int count = 0;
+char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+str = va_arg(types, char *);
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
+if (str == NULL)
+str = "(AHYY)";
+for (i = 0; str[i]; i++)
+{
+for (j = 0; in[j]; j++)
+{
+if (in[j] == str[i])
+{
+w = out[j];
+write(1, &w, 1);
+count++;
+break;
+}
+}
+if (!in[j])
+{
+w = str[i];
+write(1, &w, 1);
+count++;
+}
+}
+return (count);
+}
